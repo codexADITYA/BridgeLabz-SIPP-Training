e257534dd326cdf5a38300d81bbda41ca69432b1
Aditya saraswat <63770830+codexADITYA@users.noreply.github.com>
2025-06-16 09:45:57 +0530

    Add files via upload

---
+++ day3_PracticeProblems/assistedProblems/q4_areaOfCircle.java
@@ -0,0 +1,13 @@
+package day3_PracticeProblems.assistedProblems;
+import java.util.Scanner;
+
+public class q4_areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of the circle: ");
+        double radius = sc.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the circle: " + area);
+        sc.close();
+    }
+}
