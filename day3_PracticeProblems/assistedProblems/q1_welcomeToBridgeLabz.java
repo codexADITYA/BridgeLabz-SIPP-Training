9884b6fa4268c10b8d0d0a1e13013fc17bda5d7b
CODEXaditya <saraswat219.0@gmail.com>
2025-06-18 09:36:54 +0530

    delete day3

--- day3_PracticeProblems/assistedProblems/q1_welcomeToBridgeLabz.java
+++
@@ -1,6 +0,0 @@
-package day3_PracticeProblems.assistedProblems;
-public class q1_welcomeToBridgeLabz {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
